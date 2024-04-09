6b086af7a1746b81bb77746dede7e452d03cc58f
krampratap <krampratap4@gmail.com>
2024-04-09 13:42:54 +0530

    Singleton design pattern

---
+++ src/main/java/creational/singleton/Singleton.java
@@ -0,0 +1,30 @@
+package creational.singleton;
+
+//Eager Initialization
+public class Singleton {
+    public static void main(String[] args) {
+        //Singleton patterns allows to use single instance in the whole application.
+        //The below code gives us errors as constructor is private
+        //Database a = new Database();
+        //Database b = new Database();
+
+        Database d1 = Database.getInstance();
+        Database d2 = Database.getInstance();
+        //Both return the same object;
+    }
+}
+
+class Database {
+    //This is static because we are using it in getInstance which is a static method
+    // 1. Create a private static object of the class
+    static Database obj = new Database(); //EAGER Creation
+    // 2. Make the no args constructor as private. So that new objects are not created
+    private Database(){
+
+    }
+    //3. Create a method with any name which returns the same static object of the class.
+    public static Database getInstance()
+    {
+        return obj;
+    }
+}
---
+++ src/main/java/creational/singleton/Singleton5.java
@@ -0,0 +1,25 @@
+package creational.singleton;
+
+//ENUM will work on JAVA version > 1.5
+public class Singleton5 {
+    public static void main(String[] args) {
+        Database5 obj1 = Database5.INSTANCE;
+        obj1.i = 5;
+        obj1.show();
+
+        Database5 obj2 = Database5.INSTANCE;
+        obj2.i = 6;
+
+        obj1.show();
+    }
+}
+//Enum is a special type of class which in itself is a thread safe design pattern
+enum Database5
+{
+    INSTANCE; //By default creates a private constructor
+    int i;
+    public void show()
+    {
+        System.out.println(i);
+    }
+}
